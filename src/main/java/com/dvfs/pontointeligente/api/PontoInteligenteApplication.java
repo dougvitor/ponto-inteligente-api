5ae82b58f01dca8e60844da57dacc79fa4fe9333
Douglas Vitor <doug.vitor@gmail.com>
2019-02-12 21:52:25 -0200

    Carregando properties do arquivo application.yml ao iniciar a aplicação

--- src/main/java/com/dvfs/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/dvfs/pontointeligente/api/PontoInteligenteApplication.java
@@ -1,13 +1,23 @@
 package com.dvfs.pontointeligente.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PontoInteligenteApplication {
+public class PontoInteligenteApplication implements CommandLineRunner {
+	
+	@Autowired
+	private AcmeProperties properties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontoInteligenteApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.properties.getUri());
 	}
 
 }
