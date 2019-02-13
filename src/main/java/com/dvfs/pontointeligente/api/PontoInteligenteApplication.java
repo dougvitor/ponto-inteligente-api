b74e6baf2404b8f50b6bbfdf86bbc8d8b94cc004
Douglas Vitor <doug.vitor@gmail.com>
2019-02-13 21:57:00 -0200

    Adicionando suporte ao JPA, MySQL e H2

--- src/main/java/com/dvfs/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/dvfs/pontointeligente/api/PontoInteligenteApplication.java
@@ -1,23 +1,13 @@
 package com.dvfs.pontointeligente.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PontoInteligenteApplication implements CommandLineRunner {
+public class PontoInteligenteApplication {
 	
-	@Autowired
-	private AcmeProperties properties;
-
 	public static void main(String[] args) {
 		SpringApplication.run(PontoInteligenteApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(this.properties.getUri());
 	}
 
 }
